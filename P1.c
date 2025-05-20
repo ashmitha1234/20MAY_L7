565faa46ff373af3316d3dec9e501e807c586fe4
ashmitha1234 <ashmitha.s@s.amity.edu>
2025-05-20 11:25:43 +0530

    Added diff, sum and product

--- P1.c
+++ P1.c
@@ -4,4 +4,10 @@ int main()
 	int num1, num2;
         printf("Enter two numbers : ");
         scanf("%d %d", &num1, &num2);
+	int diff = num1 - num2;
+	printf("The differece is : %d",diff);
+	int sum = num1 + num2;
+	printf("The sum is : %d",sum);
+	int prod = num1 * num2;
+	printf("The product is : %d",prod); 
 }
